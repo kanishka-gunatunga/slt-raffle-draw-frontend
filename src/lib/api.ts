9745020cd06d9719f6905b106f31879afb8c805d
Kaveesha Sulakshana <sulakshanakaveesha@gmail.com>
2026-01-09 12:14:17 +0530

    bug fix

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:4000', // Update if deployed
+    // baseURL: 'http://localhost:4000',
+    baseURL: process.env.NEXT_PUBLIC_API_URL,
 });
 
 api.interceptors.request.use((config) => {
